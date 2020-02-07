7cfca7811fa26720fdeff611c276fcd336b9c588
Timothy Mubiru <mubstimor@gmail.com>
2020-02-07 13:34:32 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Timothy Mubiru on 2/7/20.
+//  Copyright © 2020 Timothy Mubiru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

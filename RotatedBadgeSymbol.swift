12f4a1ed6c52a001698e8c40efd113f41c197388
Timothy Mubiru <mubstimor@gmail.com>
2020-02-17 11:41:50 +0300

    draw badge shape

---
+++ RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Timothy Mubiru on 2/17/20.
+//  Copyright © 2020 Timothy Mubiru. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}

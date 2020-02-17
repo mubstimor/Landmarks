4e791129d6cb7c93eb987fb328db84299c748075
Timothy Mubiru <mubstimor@gmail.com>
2020-02-17 15:30:16 +0300

    add animations & effects

---
+++ GraphCapsule.swift
@@ -0,0 +1,37 @@
+//
+//  GraphCapsule.swift
+//  Landmarks
+//
+//  Created by Timothy Mubiru on 2/17/20.
+//  Copyright © 2020 Timothy Mubiru. All rights reserved.
+//
+
+import SwiftUI
+
+struct GraphCapsule: View {
+    var index: Int
+    var height: CGFloat
+    var range: Range<Double>
+    var overallRange: Range<Double>
+    
+    var heightRatio: CGFloat {
+        max(CGFloat(magnitude(of: range) / magnitude(of: overallRange)), 0.15)
+    }
+    
+    var offsetRatio: CGFloat {
+        CGFloat((range.lowerBound - overallRange.lowerBound) / magnitude(of: overallRange))
+    }
+    
+    var body: some View {
+        Capsule()
+            .fill(Color.white)
+            .frame(height: height * heightRatio)
+            .offset(x: 0, y: height * -offsetRatio)
+    }
+}
+
+struct GraphCapsule_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphCapsule(index: 0, height: 150, range: 10..<50, overallRange: 0..<100)
+    }
+}
---
+++ Hike.swift
@@ -0,0 +1,32 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Timothy Mubiru on 2/17/20.
+//  Copyright © 2020 Timothy Mubiru. All rights reserved.
+//
+
+import SwiftUI
+
+struct Hike: Codable, Hashable, Identifiable {
+    var name: String
+    var id: Int
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        return Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
